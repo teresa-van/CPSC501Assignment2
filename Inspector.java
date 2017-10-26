a467866ccd6f627398a0be386e652195db38c1d7
Teresa Van <teresa.van@ucalgary.ca>
2017-10-26 17:19:01 -0600

    Empty Inspector Class; No refactorings/unit tests yet.

---
+++ Inspector.java
@@ -0,0 +1,18 @@
+/*
+ * CPSC 501 Assignment #2
+ * Written by: Teresa Van (10149274)
+ * Original Code From: Jordan Kidney (Oct 23, 2005)
+ */
+
+import java.util.*;
+import java.lang.reflect.*;
+
+public class Inspector {
+	
+	public Inspector() { }
+	
+	public void inspect(Object obj, boolean recursive)
+	{
+		Class objClass = obj.getClass();
+	}
+}
